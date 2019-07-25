a9cb10f7e44eadf9c181f7d92cb52beda0112efa
Şerife Tekin <serifetekin2451@gmail.com>
2019-07-25 23:13:20 +0300

    ilk gönderim

---
+++ Form1.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace HayaletForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(23, 373);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar1.Size = new System.Drawing.Size(530, 45);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(565, 450);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HayaletForm
+{ // formlara böyle bir animasyon eklenebilir.
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            this.Opacity = 1 - trackBar1.Value / 100.0; // scrollu kaydırdıkça opacity sıfıra gelecek.
+            this.Opacity = 1 - trackBar1.Value / 100.0; // scrollu kaydırdıkça opacity sıfıra gelecek.
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Opacity = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                this.Opacity = i / 100.0;
+                System.Threading.Thread.Sleep(10); // uygulamayı 100ms durdurur, yazılım bekler.
+                Refresh(); // döngü içerisinde ekranın ve üzerindeki bileşenlerin son görünümünü gösterir.
+            }
+        }
+    }
+}
